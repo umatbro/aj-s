ffa8c1be8dd9cc64572aaac67bfb3293abb551cd
umatbro <umatbroo@gmail.com>
2017-04-13 02:28:42 +0200

    step 2

---
+++ app/app.spec.js
@@ -0,0 +1,11 @@
+describe('PhoneListController', function() {
+  beforeEach(module('phonecatApp'));
+
+  it('should create a `phones` model with 3 phones', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+
+    expect(scope.phones.length).toBe(4);
+    expect(scope.name).toBe('world');
+  }));
+});
--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     frameworks: ['jasmine'],
 
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Firefox'],
 
     plugins: [
       'karma-chrome-launcher',
